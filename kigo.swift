71872900a7d4d66b9b6feb02f29b7a0cdaa70eef
Tommy <hkrt15@icloud.com>
2024-01-27 16:57:55 +0900

    俳句アプリ化計画

--- kigo.swift
+++ kigo.swift
@@ -5,4 +5,16 @@
 //  Created by . HIKARU on 2024/01/27.
 //
 
-import Foundation
+import UIKit
+class kigo {
+    var name : String
+    var setumei : String
+    var kisetu : String
+    init(name: String, setumei: String, kisetu: String) {
+        self.name = name
+        self.setumei = setumei
+        self.kisetu = kisetu
+
+    }
+    
+}
